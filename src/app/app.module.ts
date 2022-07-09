976c9a25af6d99f667da34b8366ed1305cb69bb1
olennikovandrey <olennikovandrey@mail.ru>
2022-07-10 02:34:38 +0300

    material els into main form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { MainPageWrapperComponent } from './components/main-page-wrapper/main-page-wrapper.component';
 import { HeaderComponent } from './components/header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoteFormComponent } from './components/note-form/note-form.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageWrapperComponent,
-    HeaderComponent
+    HeaderComponent,
+    NoteFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
